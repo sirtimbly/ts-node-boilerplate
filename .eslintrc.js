cdca272ed848445de6211c9e2dc41621937d6ca1
Tim Bendt <sirtimbly@gmail.com>
2021-06-08 13:26:57 -0600

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Introduction
+A boilerplate to create a project with Node.js, Typescript, Eslint, Mocha + Chai, Dotenv
